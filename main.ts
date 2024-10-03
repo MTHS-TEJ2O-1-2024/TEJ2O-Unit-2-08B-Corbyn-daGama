d2707bc2fa490e68043486884404dddea79738e9
Corbyn-daGama <corbyn.da-gama@mths.ca>
2024-10-03 11:19:08 -0400

    Shows multiple colours for RGB LED

--- main.ts
+++ main.ts
@@ -1,8 +1,78 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Corbyn da Gama
+ * Created on: Oct 2024
+ * This program Changes the colour of the RGB LED
 */
+basic.clearScreen()
+pins.digitalWritePin(DigitalPin.P13,0)
+pins.digitalWritePin(DigitalPin.P14, 0)
+pins.digitalWritePin(DigitalPin.P15, 0)
+basic.showIcon(IconNames.Happy)
 
-basic.showString('Hello, World!')
+
+input.onButtonPressed(Button.A, function () { })
+
+
+
+//Red
+
+pins.digitalWritePin(DigitalPin.P13,1)
+basic.showString('Red')
+basic.pause(1000)
+pins.digitalWritePin(DigitalPin.P13,0)
+
+// Green
+==
+pins.digitalWritePin(DigitalPin.P14, 1)
+basic.showString('Green')
+basic.pause(1000)
+pins.digitalWritePin(DigitalPin.P14,0)
+
+
+// Blue
+
+pins.digitalWritePin(DigitalPin.P15, 1)
+basic.showString('Blue')
+basic.pause(1000)
+pins.digitalWritePin(DigitalPin.P15, 0)
+
+// Magenta
+
+pins.digitalWritePin(DigitalPin.P13,1)
+pins.digitalWritePin(DigitalPin.P15, 1)
+basic.showString('Magenta')
+basic.pause(1000)
+pins.digitalWritePin(DigitalPin.P15, 0)
+pins.digitalWritePin(DigitalPin.P13, 0)
+
+//Yellow
+
+pins.digitalWritePin(DigitalPin.P13,1)
+pins.digitalWritePin(DigitalPin.P14,1)
+basic.showString('Yellow')
+basic.pause(1000)
+pins.digitalWritePin(DigitalPin.P13, 0)
+pins.digitalWritePin(DigitalPin.P14, 0)
+
+//Cyan
+
+pins.digitalWritePin(DigitalPin.P14,1)
+pins.digitalWritePin(DigitalPin.P15,1)
+basic.showString('Cyan')
+basic.pause(1000)
+pins.digitalWritePin(DigitalPin.P14, 0)
+pins.digitalWritePin(DigitalPin.P15, 0)
+
+//white
+
+pins.digitalWritePin(DigitalPin.P13, 1)
+pins.digitalWritePin(DigitalPin.P14, 1)
+pins.digitalWritePin(DigitalPin.P15, 1)
+basic.showString('White')
+basic.pause(1000)
+pins.digitalWritePin(DigitalPin.P14, 0)
+pins.digitalWritePin(DigitalPin.P13, 0)
+pins.digitalWritePin(DigitalPin.P15, 0)
+
+
